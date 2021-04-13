47a7b851fefcf0db686f3727500a90027971390b
coraldane <coraldane@163.com>
2021-04-13 16:06:24 +0800

    ok

--- server.go
+++ server.go
@@ -112,7 +112,7 @@ func (this *serverConnection) HandleMessage(session *proto.TcpSession, msg *prot
 	case proto.REQUEST:
 		tcpRequest := &proto.TcpRequest{}
 		tcpRequest.Decode(msg.Body)
-		logger.Debug("Request>>>===userId: %d, msgId: %s, flag: %v, method: %d, body: %s", session.GetUserId(), msg.MsgId, msg.Flag, tcpRequest.Method, string(tcpRequest.Data))
+		logger.Debug("Request>>>===userId: %d, msgId: %s, flag: %v, method: %s, body: %s", session.GetUserId(), msg.MsgId, msg.Flag, tcpRequest.Method, string(tcpRequest.Data))
 
 		if "" == tcpRequest.Method {
 			session.WriteError(msg, "method name empty")
